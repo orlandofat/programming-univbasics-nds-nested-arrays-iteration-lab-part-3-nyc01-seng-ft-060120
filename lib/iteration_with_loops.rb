bf2a0e4c4545478b85c521e36b1e792032eace80
Orlando Challenger <orlandofat@gmail.com>
2020-05-20 02:31:36 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
    # binding pry
-   if src[row_index][element_index].class == String
+   if src[row_index][element_index].class.join == String
      nested_string << src[row_index][element_index] 
    end
     element_index += 1 
